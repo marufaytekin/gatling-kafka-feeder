a7aedb5782c1991b0968901eaba10a5edc3b9ec9
Maruf Aytekin <maruf.aytekin@jet.com>
2019-06-25 21:21:56 -0400

    gatling kafka feeder commit

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "KafkaStreams"
+name := "gatling-kafka-feeder"
 organization        := "com.jet.guardians.gatling"
 version             := "1.0-SNAPSHOT"
 
--- src/main/scala/com/jet/guardians/gatling/Consumer.scala
+++ src/main/scala/com/jet/guardians/gatling/Consumer.scala
@@ -1,5 +1,6 @@
 import java.util
 import java.util.Properties
+import scala.util.control.Breaks._
 
 import org.apache.kafka.clients.consumer._
 import scala.collection.JavaConverters._
@@ -7,28 +8,42 @@ import scala.collection.JavaConverters._
 object Consumer {
 
   def main(args: Array[String]): Unit = {
-    consumeFromKafka("bank-transactions")
-
+    val numOfMessages = 1000
+    val consumerGroupId: String = "gatling-consumer"
+    val data = consumeFromKafka("localhost:9092", "bank-transactions", consumerGroupId, numOfMessages)
+    println(data.length)
+    for (item <- data) {
+      println(item)
+    }
   }
 
-  def consumeFromKafka(topic: String): Unit = {
+  def consumeFromKafka(servers: String, topic: String, groupId: String, n: Int=0): List[Object] = {
     val config: Properties = new Properties()
-
-    config.put("bootstrap.servers", "localhost:9092")
+    config.put("bootstrap.servers", servers)
     config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     config.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
     config.put("auto.offset.reset", "earliest")
-    config.put("group.id", "consumer-group")
+    config.put("group.id", groupId)
 
     val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](config)
     consumer.subscribe(util.Arrays.asList(topic))
-    while (true) {
-      val records = consumer.poll(100).asScala
-      for (record  <- records) {
-        println(record)
-      }
-      consumer.commitAsync
-    }
+    var data = List[Object]()
 
+    breakable {
+      while (true) {
+        val records = consumer.poll(100).asScala
+        for (record <- records) {
+          //println(record)
+          //println(record.key(), record.value())
+          data = data :+ record
+          if (n != 0 && data.length > n-1) {
+            consumer.commitAsync
+            break()
+          }
+        }
+        consumer.commitAsync
+      }
+    }
+    data
   }
 }
